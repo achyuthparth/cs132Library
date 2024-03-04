1d7360c548431f3076cd7fe8bce21fc0875ebdee
achyuthparth <achyuthaa@outlook.com>
2024-03-03 19:49:36 -0500

    saving work

--- item_storage.py
+++ item_storage.py
@@ -1,17 +1,8 @@
-class Item: # abstract class
+class Book:
     title: str
     details : str
     id: int
-    def details(self):
-        return self.details
-
-class Book(Item):
     author : str
     isbn : str
-
-class DVD(Item):
-    director : str
-    isbn : str
-
-class Misc(Item):
-    pass
+    def details(self):
+        return self.details
--- library.py
+++ library.py
@@ -9,16 +9,7 @@ from item_storage import Book, DVD, Misc
 class Book_storage(OrderedDict): # key will be item name, value will be frequency
     pass
 
-class DVD_storage(OrderedDict):
-    def checkout(): # try to reduce frequency, if it does not work then throw exception for checked out
-        return
-
-class Misc_storage(OrderedDict):
-    pass
-
 class Library: # generate reports
     books: Book_storage
-    DVDs : DVD_storage
-    misc : Misc_storage
     patrons : set
     librarians : set
