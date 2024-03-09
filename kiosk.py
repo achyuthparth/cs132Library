77683699c2b48301f5f3615086f09c3b46564279
achyuthparth <achyuthaa@outlook.com>
2024-03-09 12:23:17 -0500

    fixing commenting

--- kiosk.py
+++ kiosk.py
@@ -29,59 +29,65 @@ class Kiosk: # add methods for checking permissions
         return False
 
     def checkout_item(self, item, user):
-# Validate item and customer type
-        if not(isinstance(item, Book)):
-            print("Checkout item is not a book")
-            raise Not_Book
+        # Validate customer type
         if not(isinstance(user, User)):
             print("Checkout user is not a user")
             raise Not_User
-# validate if user has permissions
+        
+        # validate if user has permissions
         if not(self.check_permissions(user, "checkout_books")):
             print("Does not have permission to checkout books")
             raise PermissionError("Does not have permission to checkout books")
-# create new transaction
+        
+        # Validate book type
+        if not(isinstance(item, Book)):
+            print("Checkout item is not a book")
+            raise Not_Book
+        
+        # create new transaction
         new_transaction = Transaction(user.id, item.id)
         self.transaction_store.add_transaction(new_transaction)
         receipt = f"{new_transaction.item_id} {new_transaction.customer_id} {new_transaction.checkout_date}"
         print(f"Your receipt is {receipt}")
-# remove the book from the item storage
+        
+        # remove the book from the item storage
         self.book_store.remove_book(item)
-        return receipt
-    
-    def checkout_item2(self, item, customer): # built for debugging transaction file services
-        new_transaction = Transaction(customer.id, item.id)
-        self.transaction_store.add_transaction(new_transaction)
-        receipt = f"{new_transaction.item_id} {new_transaction.customer_id} {new_transaction.checkout_date}"
         return receipt
         
     def return_item(self, receipt):
-# find the transaction from the storage
+        # find the transaction from the storage
         transaction = self.transaction_store.find_transaction(receipt)
-# change the return date
+        
+        # change the return date
         transaction.return_date = datetime.datetime.utcnow()
         self.transaction_store.write_file()
-# find the isbn for the book from the transaction details
+        
+        # find the isbn for the book from the transaction details
         book_id = transaction.item_id
-# find the book object from the storage using the isbn
+        
+        # find the book object from the storage using the isbn
         book = self.book_store.find_isbn(book_id)
-# change details in the storage to book returned
+        
+        # change details in the storage to book returned
         self.book_store.return_book(book)
         return f"{receipt} {transaction.return_date}"
     
     def create_patron(self, user, name, email, number):
-# Type check
+        # Type check
         if not(isinstance(user, User)):
             print("User is not a user type")
             raise Not_User
-# permission check
+        
+        # permission check
         if not(self.check_permissions(user, "add_patrons")):
             print("Does not have permission to add patrons")
             raise PermissionError("Does not have permission to add patrons")
-# creating patron
+        
+        # creating patron
         print("Creating new user")
         new_patron = User(name, email, number)
-# setting roles for patron
+        
+        # setting roles for patron
         print("Adding patron role")
         new_patron.add_role(Role("Patron", ["checkout_books", "return_books"]))
         print("Saving user")
@@ -89,102 +95,119 @@ class Kiosk: # add methods for checking permissions
         return new_patron.id
     
     def delete_patron(self, user, patron_id):
-# type validation
+        # type validation
         if not(isinstance(patron_id, str)):
             print("enter the patron ID as a string")
             raise TypeError("Enter a string")
         if not(isinstance(user, User)):
             print("User is not a user type")
             raise Not_User
-# permission validation
+        
+        # permission validation
         if not(self.check_permissions(user, "delete_patrons")):
             print("Does not have permission to delete patrons")
             raise PermissionError("Does not have permission to delete patrons")
-# finding user
+        
+        # finding user
         patron = self.user_store.find_user_id(patron_id)
-# deleting user
+        
+        # deleting user
         print("Deleting patron")
         self.user_store.remove_user(patron)
         return patron_id
     
     def add_librarian(self, user, name, email, number):
-# Type check
+        # Type check
         if not(isinstance(user, User)):
             print("User is not a user type")
             raise Not_User
-# permission check
+        
+        # permission check
         if not(self.check_permissions(user, "add_librarians")):
             print("Does not have permission to add librarians")
             raise PermissionError("Does not have permission to add librarians")
-# creating librarian account
+        
+        # creating librarian account
         print("Creating new user")
         new_librarian = User(name, email, number)
         print("Adding librarian role")
-# adding librarian role
+        
+        # adding librarian role
         new_librarian.add_role(Role("Librarian", ["add_patrons", "add_librarians", "remove_patrons", "remove_librarians", "add_books", "remove_books"]))
-# saving librarian account
+        
+        # saving librarian account
         print("Saving user")
         self.user_store.add_user(new_librarian)
         return new_librarian.id
     
     def delete_librarian(self, user, librarian_id):
-# type validation
+        # type validation
         if not(isinstance(librarian_id, str)):
             print("enter the librarian ID as a string")
             raise TypeError("Enter a string")
         if not(isinstance(user, User)):
             print("User is not a user type")
             raise Not_User
-# permission validation
+        
+        # permission validation
         if not(self.check_permissions(user, "delete_librarians")):
             print("Does not have permission to delete librarians")
             raise PermissionError("Does not have permission to delete librarians")
-# finding user
+        
+        # finding user
         librarian = self.user_store.find_user_id(librarian_id)
-# deleting user
+        
+        # deleting user
         print("Deleting patron")
         self.user_store.remove_user(librarian)
         return librarian_id
     
     def add_book(self, user, title, author, isbn, quantity = 1):
-# type validation
+        # type validation
         if not(isinstance(user, User)):
             print("User is not a user type")
             raise Not_User
-# permission validation
+        
+        # permission validation
         if not(self.check_permissions(user, "add_books")):
             print("Does not have permission to add books")
             raise PermissionError("Does not have permission to add books")
-# adding book to storage
+        
+        # adding book to storage
         print("adding book")
         self.book_store.add_book(Book(title, author, isbn, quantity))
         return isbn
     
     def delete_book(self, user, book_isbn):
-# type validation
+        # type validation
         if not(isinstance(user, User)):
             print("User is not a user type")
             raise Not_User
         if not(isinstance(book_isbn, str)):
             raise TypeError("Enter string for book ISBN")
-# permission validation
+        
+        # permission validation
         if not(self.check_permissions(user, "delete_books")):
             print("Does not have permission to delete books")
             raise PermissionError("Does not have permission to delete books")
-# finding the book
+        
+        # finding the book
         print("finding book")
         book = self.book_store.find_isbn(book_isbn)
-# removing book from storage
+        
+        # removing book from storage
         print("deleting book")
         self.book_store.delete_book(book)
         return book_isbn
     
     def book_details(self, book_isbn):
-# type validation
+        # type validation
         if not(isinstance(book_isbn, str)):
             raise TypeError("Enter string for book ISBN")
-# finding the book
+        
+        # finding the book
         print("finding book")
         book = self.book_store.find_isbn(book_isbn)
-# returning the book details
+        
+        # returning the book details
         return book.details()
--- library.py
+++ library.py
@@ -3,6 +3,56 @@
 from tkinter import *
 import kiosk
 
+root = Tk()
+root.title("Achyuthaa's Library")
 class Library(Tk):
     def __init__(self, screenName: str | None = None, baseName: str | None = None, className: str = "Tk", useTk: bool = True, sync: bool = False, use: str | None = None) -> None:
         super().__init__(screenName, baseName, className, useTk, sync, use)
+        container = Frame(self)
+        container.pack(side = 'top', fill = 'both', expand = True)
+        self.Frames = {}
+# frames needed are login frame, action choice frame, action frame
+        for F in (Login_Frame, Patron_Frame, Librarian_Frame, Checkout_Frame, Return_Frame, Create_Patron_Frame, Delete_Patron_Frame, Add_Librarian_Frame, Delete_Librarian_Frame, Add_Book_Frame, Delete_Book_Frame, Book_Details_Frame):
+            frame = F(container, self)
+            frame.grid(row=0, colomn=0)
+        self.show_frame(Login_Frame)
+        goHomeButton = Button(self,
+            text = "Go Home",
+            command = lambda: self.show_frame(Login_Frame))
+        goHomeButton.pack()
+    
+    def show_frame(self, frame_type_to_show):
+        frame = self.Frames[frame_type_to_show]
+        frame.Reset()
+        frame.tkraise()
+
+class Login_Frame(Frame):
+    def __init__(self, parent, application):
+        self.Application = application
+        Frame.__init__(self, parent)
+        startFrame = LabelFrame(self, text = "Welcome to the Library")
+    variable_login = StringVar()
+    login_input = Entry(root, width = 50, borderwidth = 5)
+    login_input.grid(row = 3, column = 4, columnspan = 3, padx = 10, pady = 10)
+    login_input.insert("Enter user ID")
+    user_id = login_input.get()
+    login_button = Button(root, text = "Login")
+    
+    variable_book_details = StringVar()
+    find_book_details = Button(root, text = "Find book details using ISBN", variable = variable_book_details)
+    
+
+
+class Patron_Frame(Frame): pass
+class Librarian_Frame(Frame): pass
+class Checkout_Frame(Frame): pass
+class Return_Frame(Frame): pass
+class Create_Patron_Frame(Frame): pass
+class Delete_Patron_Frame(Frame): pass
+class Add_Librarian_Frame(Frame): pass
+class Delete_Librarian_Frame(Frame): pass
+class Add_Book_Frame(Frame): pass
+class Delete_Book_Frame(Frame): pass
+class Book_Details_Frame(Frame): pass
+
+root.mainloop()
